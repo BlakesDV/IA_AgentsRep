f133a04b3b786fc6aa98a1af4fd9a5079f3c38d1
Blakes <dmunoz@cocoschool.mx>
2024-10-29 17:11:05 -0600

    update canvas

--- Agents/Assets/AvoidDetection/Code/LevelReferee.cs
+++ Agents/Assets/AvoidDetection/Code/LevelReferee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using SotomaYorch.FiniteStateMachine;
 using SotomaYorch.FiniteStateMachine.Agents;
+using TMPro;
 
 namespace SotomaYorch.AvoidDetection
 {
